f79d880393af44dfd318b81e79f2e952cf3a2e05
NicholasMiron <nicholasmiron@gmail.com>
2019-03-14 11:24:39 -0500

    Made the sql not burn to the ground

--- server/schema.sql
+++ server/schema.sql
@@ -1,31 +1,28 @@
--- CREATE DATABASE chat;
+DROP DATABASE IF EXISTS chat;
+CREATE DATABASE chat;
 
 USE chat;
+
+CREATE TABLE rooms (
+  room_id int,
+  name text(63) NOT NULL,
+  PRIMARY KEY (room_id)
+);
+
+CREATE TABLE users (
+  user_id int,
+  name text(63) NOT NULL,
+  PRIMARY KEY (user_id)
+);
 
 CREATE TABLE messages (
   message_id int(255) NOT NULL AUTO_INCREMENT,
   message_text text(511) NOT NULL,
   message_user int(255),
   message_room int(255),
-  PRIMARY KEY (message_id)
-  -- FOREIGN KEY (message_user) REFERENCES users(user_id),
-  -- FOREIGN KEY (message_room) REFERENCES rooms(room_id)
-);
-
-
-
-/* Create other tables and define schemas for them here! */
-
-CREATE TABLE rooms (
-  room_id int,
-  name string(63) NOT NULL,
-  PRIMARY KEY (room_id)
-);
-
-CREATE TABLE users (
-  user_id int,
-  name string(63) NOT NULL,
-  PRIMARY KEY (user_id)
+  PRIMARY KEY (message_id),
+  FOREIGN KEY (message_user) REFERENCES users(user_id),
+  FOREIGN KEY (message_room) REFERENCES rooms(room_id)
 );
 
 CREATE TABLE message_rooms (
@@ -41,6 +38,8 @@ CREATE TABLE message_users (
   FOREIGN KEY (message_id) REFERENCES messages(message_id),
   FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
+
+
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
